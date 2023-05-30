f45b71851ae88ead8fa3e36e24568406d4f643d0
Andrew Tran <andrew.tran@gimbal.com>
2023-05-30 16:18:43 -0700

    Fix Package.swift binary target definition.

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .target(name: "Gimbal", dependencies: [])
+        .binaryTarget(name: "Gimbal", path: "Sources/Gimbal.xcframework")
     ]
 )
